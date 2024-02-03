866c8b9a744e4c9d930d99dfc53a3759b48be2d4
LEMUS123l <jonatanpadilla247@gmail.com>
2024-02-03 15:05:04 -0600

    clase 3

---
+++ datosusuario.py
@@ -0,0 +1,4 @@
+#solicitar el nombre y la edad de un usuario y mostrarlos en pantalla
+nombre=input("ingrese nombre: ")
+edad=int(input("ingrese edad: "))
+print("bienbenido",nombre,"su edad es de",edad)
---
+++ entrada.py
@@ -0,0 +1,11 @@
+#print("hola")
+#input("ingrese su nombre")
+#edadpersona="65"
+#print(edadpersona)
+#nombre=input("ingrese nombre: ")
+#print(nombre)
+#sumar dos cantidades
+n1=int(input("ingrese primer cantidad: "))
+n2=int(input("ingrese sugunda cantidd: "))
+suma=n1+n2
+print(suma)
